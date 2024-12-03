4f5aa65efae357264cfc82a0d990a9bb15b47039
Rakibul Islam <rakibulislam10.net@gmail.com>
2024-12-03 17:55:58 +0600

    updated code

--- lib/views/product_details/controller/product_details_controller.dart
+++ lib/views/product_details/controller/product_details_controller.dart
@@ -1,27 +1,16 @@
-import 'package:get/get.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 import '../../../base/utils/basic_import.dart';
 
 class ProductDetailsController extends GetxController {
-
   Rx<CameraPosition> initialCameraPosition = CameraPosition(
     target: LatLng(37.7749, -122.4194), // Example coordinates (San Francisco)
     zoom: 16.0,
   ).obs;
-  //
-  //
-  // ScrollController scrollController = ScrollController();
-  // void smoothScrollTo(double offset) {
-  //   scrollController.animateTo(
-  //     offset,
-  //     duration: Duration(seconds: 1),
-  //     curve: Curves.easeInOut,
-  //   );
-  // }
-  // @override
-  // void onClose() {
-  //   scrollController.dispose();
-  //   super.onClose();
-  // }
+
+  var currentPage = 0.obs;
+  final List<String> imageUrls = [
+    'https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.webp?b=1&s=612x612&w=0&k=20&c=V8oaDpP3mx6rUpRfrt2L9mZCD0_ySlnI7cd4nkgGAb8=',
+    'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
+    'https://www.allrecipes.com/thmb/fFW1o307WSqFFYQ3-QXYVpnFj6E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg',
+    'https://plus.unsplash.com/premium_photo-1673108852141-e8c3c22a4a22?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZCUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D'
+  ];
 }
--- lib/views/product_details/widgets/image_widgets.dart
+++ lib/views/product_details/widgets/image_widgets.dart
@@ -1,21 +1,65 @@
 import '../../../base/utils/basic_import.dart';
 import 'back_arrow_button.dart';
 
-class ImageWidgets extends StatelessWidget {
-  const ImageWidgets({super.key});
+class ImageWidgets extends GetView<ProductDetailsController> {
+  ImageWidgets({super.key});
+
+  final PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      leading: BackArrowButton(),
-      pinned: true,
-      expandedHeight: MediaQuery.of(context).size.height * 0.356,
-      backgroundColor: Colors.transparent,
-      scrolledUnderElevation: 0,
-      flexibleSpace: FlexibleSpaceBar(
-          collapseMode: CollapseMode.none,
-          background: Image.network(
-              'https://www.allrecipes.com/thmb/fFW1o307WSqFFYQ3-QXYVpnFj6E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg')),
-    );
+        leading: BackArrowButton(),
+        pinned: true,
+        expandedHeight: MediaQuery.of(context).size.height * 0.356,
+        backgroundColor: Colors.transparent,
+        scrolledUnderElevation: 0,
+        flexibleSpace: Stack(
+          alignment: Alignment.topLeft,
+          children: [
+            FlexibleSpaceBar(
+              collapseMode: CollapseMode.none,
+              background: PageView.builder(
+                controller: pageController,
+                onPageChanged: (index) {
+                  controller.currentPage.value = index;
+                },
+                itemCount: controller.imageUrls.length,
+                itemBuilder: (context, index) {
+                  return Image.network(
+                    controller.imageUrls[index],
+                    fit: BoxFit.cover,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) {
+                        return child;
+                      }
+                      return Loader();
+                    },
+                  );
+                },
+              ),
+            ),
+            Positioned(
+              right: Dimensions.heightSize * 1.5,
+                top: Dimensions.heightSize * 1.5,
+                child: Obx(
+                  () => Row(
+                      children: List.generate(
+                    controller.imageUrls.length,
+                    (index) => Container(
+                      margin: EdgeInsets.symmetric(horizontal: 4),
+                      height: Dimensions.heightSize,
+                      width: Dimensions.heightSize,
+                      decoration: BoxDecoration(
+                          color: controller.currentPage.value == index
+                              ? CustomColor.primary
+                              : CustomColor.whiteColor,
+                        shape: BoxShape.circle
+                          ),
+                    ),
+                  )),
+                ))
+          ],
+        ));
   }
 }
